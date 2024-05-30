bd9a669e87b66d60809889b7ba1b70d1f6b83558
Athhar Kautsar <athharkautsar14@gmail.com>
2024-05-30 08:50:15 +0700

    feat(core): add cors and changing validation rules

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_mail import Mail
 from app.blueprint import Blueprint
 from app.config import ErrorHandler
@@ -10,6 +11,7 @@ from app.utility.string import str_to_bool
 
 def create_app():
     app = Flask(__name__, static_folder=None)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     app.config.from_mapping(
         CELERY=dict(
@@ -25,5 +27,14 @@ def create_app():
     MailHandler.init(app)
     ErrorHandler.registerErrorHandler(app)
     Blueprint.register(app)
+
+    def after_request(response):
+        header = response.headers
+        header["Access-Control-Allow-Origin"] = "*"
+        header["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        header["Access-Control-Allow-Methods"] = "OPTIONS, HEAD, GET, POST, DELETE, PUT"
+        return response
+
+    app.after_request(after_request)
 
     return app
